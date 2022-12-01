4d7da91cceaec5a81892b49fa12f1cd78ff681d0
victor okwuosa <victorudenna@gmail.com>
2022-12-01 19:03:08 +1300

    routed across all pages

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,18 +5,19 @@ import { RiRadioFill, RiVideoLine } from "react-icons/ri";
 import { IoPerson, IoLogOutOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import LogoImg from "../../assets/images/logo.svg";
-// import { Collections } from "../../pages/Collections/";
 
 const SideBar = () => {
   return (
     <Fragment>
       <div className="py-4 px-5">
         <div className="">
-          <img src={LogoImg} alt="Logo" />
+         <Link to='./'>
+         <img src={LogoImg} alt="Logo" />
+         </Link>
         </div>
         <ul className="mt-6">
           <div className="bg-[#000000]/30 text-[#EFEEE0]/50 text-center px-3 rounded-[20px] mb-5 mt-4">
-            <Link className="flex items-center py-[15px] cursor-pointer hover:text-yellow-400">
+            <Link to='./DashBoard' className="flex items-center py-[15px] cursor-pointer hover:text-yellow-400">
               <AiOutlineHome size={30} />
             </Link>
             <Link to ="./Collections" className="flex items-center py-[15px] cursor-pointer hover:text-yellow-400">
