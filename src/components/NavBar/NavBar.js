8d7a48a408eedc85176b10475e332121b577f87e
victor okwuosa <victorudenna@gmail.com>
2022-12-06 05:35:32 +1300

    addedn onclick function to close dashboard sidebar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <Fragment>
       {/* desktop Navigation Bar */}
-      <div className="hidden md:block p-5 ">
+      <div className="hidden md:block p-5">
         <SearchBar
           type={"text"}
           className={"bg-transparent focus:outline-none font-[14px]"}
@@ -36,37 +36,37 @@ const NavBar = () => {
         </div>
       </div>
       {/* side menu navigation */}
-      <div
+      <div onClick={() => setNav(!nav)}
         className={
-          nav ? "fixed top-0 left-0 h-screen w-[350px] bg-dark z-10 duration-1000 md:hidden" : "fixed top-0 left-[-1000px] h-screen w-[350px] bg-white z-10 duration-1000"
+          nav ? "fixed top-0 left-0 h-screen w-[350px] bg-dark z-10 duration-1000 md:hidden cursor-pointer" : "fixed top-0 left-[-1000px] h-screen w-[350px] bg-white z-10 duration-1000 cursor-pointer"
         }
       >
         <ul className=" text-light/50 pt-[80px] pl-[25px]">
-          <Link to ='./DashBoard'  className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400">
+          <Link to ='./DashBoard'  className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400 w-[50%]">
             <span className="px-3">
               <AiOutlineHome size={30} />
             </span>
             <span className="font-bold text-[17px]">Home</span>
           </Link>
-          <Link to= "./Collections" className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400">
+          <Link to= "./Collections" className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400 w-[60%]">
             <span className="px-3">
               <BsCollectionPlay size={30} />
             </span>
             <span className="font-bold text-[17px]">My Collections</span>
           </Link>
-          <Link className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400">
+          <Link className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400 w-[50%]">
             <span className="px-3">
               <RiRadioFill size={30} />
-            </span>
+            </span> 
             <span className="font-bold text-[17px]">Radio</span>
           </Link>
-          <Link className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400">
+          <Link className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400 w-[50%]">
             <span className="px-3">
               <RiVideoLine size={30} />
             </span>
             <span className="font-bold text-[17px]">Music Videos</span>
           </Link>
-          <Link className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400">
+          <Link className="flex items-center py-[25px] cursor-pointer hover:text-yellow-400 w-[50%]">
             <span className="px-3">
               <IoPerson size={30} />
             </span>
